d6821d427be97c156f6b22bcadd7a656ce333e88
Graham <gparvinkarimi@captechventures.com>
2020-03-24 17:25:57 -0400

    Add instructions for providing feedback

--- ct-component-library-app/src/App.js
+++ ct-component-library-app/src/App.js
@@ -13,13 +13,32 @@ function App() {
       <div>
         <p className={cn({ header: true })}>
           CapTech Component Library Documentation (accessible on CapTech Network
-          or VPN):{" "}
+          or VPN): <br></br>
           <a
             className={cn({ link: true })}
             href="http://si.captech.site/ct-react-library/"
           >
             http://si.captech.site/ct-react-library/
           </a>
+        </p>
+      </div>
+      <div>
+        <p className={cn({ header: true })}>
+          Please provide feedback on your experience by filling out{" "}
+          <a
+            className={cn({ link: true })}
+            href="https://teams.microsoft.com/_?tenantId=ae9d6e9a-cc18-4204-ac29-43a0ccb860e8#/tab::c58eadc6-6a68-43bd-95fb-112c48ebaed7/Component%20Library%20Feedback?threadId=19:df9128676cd640eea561ed01e0528bff@thread.skype&ctx=channel"
+          >
+            this Component Usability Feedback survey on Teams
+          </a>{" "}
+          and also please feel free to post any feedback in{" "}
+          <a
+            className={cn({ link: true })}
+            href="https://teams.microsoft.com/l/channel/19%3adf9128676cd640eea561ed01e0528bff%40thread.skype/Component%2520Library%2520Feedback?groupId=cc19cb29-06a2-48e4-adcc-b6e4a8fe7dfa&tenantId=ae9d6e9a-cc18-4204-ac29-43a0ccb860e8"
+          >
+            the Component Usability Teams channel
+          </a>
+          .
         </p>
       </div>
       <p className={cn({ header: true })}>
